28d945c267b820750dd93f4ff939af6fcfb415c1
Myloveiszero <2446730006@qq.com>
2020-06-13 21:42:09 +0800

    音乐播放器

---
+++ mango-music-chapter/src/components/album/Album.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './album.styl';
+
+export default class Album extends Component {
+    render() {
+        return (
+            <div>
+                album
+            </div>
+        )
+    }
+}
