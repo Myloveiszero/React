7238cc16da5b58db40d86831d03038b33871397b
Myloveiszero <2446730006@qq.com>
2020-07-17 19:03:20 +0800

    知识星球

--- mobile-qunar/src/App.js
+++ mobile-qunar/src/App.js
@@ -1,28 +1,37 @@
-import React,{ useCallback } from 'react';
-import logo from './logo.svg';
+import React,{ useCallback, useMemo } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import Header from './components/header/Header.jsx';
 import Journey from './components/journey/Journey.jsx';
-
+import { bindActionCreators } from 'redux';
+import {
+  exchangeFromTo
+} from './store/actions';
 
 function App(props) {
   const { 
     from,
-    to
+    to,
+    dispatch
   } = props;
 
   const onBack = useCallback(() => {
       window.history.back();
     },[])
 
+    const cbs = useMemo(() => {
+      return bindActionCreators({
+        exchangeFromTo
+      },dispatch)
+    },[])
+
   return (
     <div className="App">
-     <div class="header-wrapper">
+     <div className="header-wrapper">
        <Header title="火车票" onBack={onBack}/>
      </div>
      <form action="./query.html" className="form">
-      <Journey from={from} to={to} />
+      <Journey from={from} to={to} {...cbs} />
      </form>
     </div>
   );
@@ -32,4 +41,8 @@ function App(props) {
 export default connect(function mapStateToProps(state) {
   //状态的读操作
   return state;
+},function mapDispatchToProps(dispatch) {
+  return {
+    dispatch
+  }
 })(App);
--- mobile-qunar/src/components/header/Header.jsx
+++ mobile-qunar/src/components/header/Header.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
-import Journey from '../journey/Journey';
+
 
 export default function Header(props) {
     const { title,onBack } = props;
 
     return (
         <div className="header">
-            <div class="header-back" onClick={onBack}>
+            <div className="header-back" onClick={onBack}>
                 <svg width="42" height="42">
                     <polyline points="25,13 16,21 25,29" stroke="#fff" strokeWidth="2" fill="none"/>
                 </svg>
             </div>
-            <div class="header-title">{title}</div>
+            <div className="header-title">{title}</div>
         </div>
     )
 }
 Header.propTypes = {
-    from: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    onBack: PropTypes.func.isRequired
 }
